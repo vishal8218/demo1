346950c9dc195c40e3027ef03326eb45049051a5
Vishal Kumar <67638997+vishal8218@users.noreply.github.com>
2023-02-04 15:18:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+  function application()
+  {
+    alert("Details store successfully!")
+  }
+  function run()
+  {
+    var url="https://www.youtube.com/watch?v=R3PspP7-lao";
+        window.open(url,null);
+  }
+  function call(something)
+  {
+     console.log("Welcome to AJAX");
+  }
+  
+function myCalculator(num1, num2, myCallback) {
+  let sum = num1 + num2;
+  myCallback(sum);
+  console.log(sum);
+}
+
+myCalculator(5, 5, call);
+ 
+  function multi(a,b)
+  {
+    alert(a*b);
+  }
+function but1(a)
+{
+  alert( a*a);
+}  
+function but2(a)
+{
+  alert(a*a);
+}  
+  
+function but3(a)
+{
+  alert(a*a);
+
+}  
+  
+function but4(a)
+{
+  alert(a*a);
+}  
+  
+function but5(a)
+{
+  alert(a*a);
+}  
+  
+function but6(a)
+{
+  aler(a*a);
+}  
+  
+function but7(a)
+{
+  alert(a*a);
+}  
+  
+function but8()
+{
+  alert(a*a);
+}  
+  
+function but9(a)
+{
+  alert(a*a);
+}  
+
